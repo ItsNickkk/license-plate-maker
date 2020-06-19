88c477dc7cebdee405977c34e70b8b1d14410515
ItsNickkk <48113405+ItsNickkk@users.noreply.github.com>
2020-06-19 15:57:20 +0800

    Make LTO Abbr, Hiragana, Class No, and Number into individual component

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 class Plate:
 
+	#List of plate type
 	PLATE_TYPE = {
 		1: "resource/template/normal.png",
 		2: "resource/template/kei.png",
@@ -17,6 +18,7 @@ class Plate:
 		4: "resource/template/kei-comm.png"
 	}
 
+	#List of font color
 	FONT_COLOR = {
 		1: (25,79,56),
 		2: (50,50,50),
@@ -24,28 +26,45 @@ class Plate:
 		4: (243,194,4)
 	}
 
+	#List of font directories
+	LIST_FONT = {
+		"TRM": "resource/font/trm.ttf",
+		"FZ": "resource/font/fz.otf"
+	}
+
 	#List of Hiragana by Font
 	FONT_TRM_HIRA = list("あいうかきくけこせを")
 	FONT_FZ_HIRA = list("えさすそたちつてとなにぬねのはひふほまみむめもやゆよらりるれろわ")
 
+	#List of LTO Abbreviations
+	LIST_TRM_LTO_ABBR = ["ナニワ", "山口", "岐阜", "石川", "浜松", "島根", "横浜", "Ｏ", "Ｉ"]
+	LIST_FZ_LTO_ABBR = ["ツクバ", "福島", "北九州", "奈良", "徳島", "金沢", "山口", "富士山", "高知"]
+
+	
+
 	def __init__(self, p_lto_abbr, p_class_num, p_hira, p_number, p_type: int=1):
-		self.class_num = p_class_num
 		self.type = self.PLATE_TYPE[p_type]
+		self.font_color = self.FONT_COLOR[p_type]
 		#Hiragana Character Setting
 		self.hira = p_hira
 		self.hira_font = self._hira_font(p_hira)
 		self.hira_font_size = 650 if self.hira in self.FONT_TRM_HIRA else 200
 		self.hira_position = (60, 180) if self.hira in self.FONT_TRM_HIRA else (85, 380)
-		#LTO abbreviation setting
+		#LTO abbreviation Setting
 		self.lto_abbr = p_lto_abbr
-		self.lto_abbr_font_size = 200 if self.hira in self.FONT_TRM_HIRA else 150
-		#Other
-		self.font_color = self.FONT_COLOR[p_type]
+		self.lto_abbr_font = self._lto_abbr_font(self.lto_abbr)
+		self.lto_abbr_font_size = 200 if self.lto_abbr in self.LIST_TRM_LTO_ABBR else 175
+		self.lto_abbr_position = (340, 70) if self.lto_abbr in self.LIST_TRM_LTO_ABBR else (365, 70)
+		#Class Number Setting
+		self.class_num = p_class_num
+		self.class_num_font = self.LIST_FONT["TRM"]
+		#Plate Number Setting	
 		self.number = p_number
+		self.number_font = self.LIST_FONT["TRM"]
 
 	def __str__(self):
 		return f"""
-		LTO Abbreviation: {self.lto_abbr}
+		LTO Abbreviation: {self.lto_abbr}, Font used: {self.lto_abbr_font}
 		Class Number: {self.class_num}
 		Hiragana Character: {self.hira}, Font used: {self.hira_font}
 		Number: {self.number}
@@ -54,22 +73,37 @@ class Plate:
 
 	def _hira_font(self, letter):
 		if letter in self.FONT_TRM_HIRA:
-			return "resource/font/trm.ttf"
+			return self.LIST_FONT["TRM"]
 		elif letter in self.FONT_FZ_HIRA:
-			return "resource/font/fz.otf"
+			return self.LIST_FONT["FZ"]
 		else:
 			raise HiraganaNotFoundError
+			return "null"
+
+	def _lto_abbr_font(self, lto_abbr):
+		if lto_abbr in self.LIST_TRM_LTO_ABBR:
+			return self.LIST_FONT["TRM"]
+		elif lto_abbr in self.LIST_FZ_LTO_ABBR:
+			return self.LIST_FONT["FZ"]
+		else:
+			raise LTOAbbreviationNotFoundError
 			return "null"
 
 	def generatePlate(self):
 		img = Image.open(self.type)
 		draw = ImageDraw.Draw(img)
 		#Draw LTO Abbreviation
-		#font_lto_abbr = ImageFont.truetype(self.hira_font, self.hira_font_size)
-		#draw.text((340, 70),self.lto_abbr+"＠"+self.class_num,fill=self.font_color, font=font_lto_abbr)
+		font_lto_abbr = ImageFont.truetype(self.lto_abbr_font, self.lto_abbr_font_size)
+		draw.text(self.lto_abbr_position,self.lto_abbr,fill=self.font_color, font=font_lto_abbr)
 		#Draw Hiragana Character
 		font_hira = ImageFont.truetype(self.hira_font, self.hira_font_size)
 		draw.text(self.hira_position,self.hira,fill=self.font_color, font=font_hira)
+		#Draw Class Number
+		font_class_num = ImageFont.truetype(self.class_num_font, 200)
+		draw.text((770, 70), self.class_num, fill=self.font_color, font=font_class_num)
+		#Draw Number
+		font_number = ImageFont.truetype(self.number_font, 400)
+		draw.text((335, 300), self.number, fill=self.font_color, font=font_number)
 		img.save("test.png")
 
 class Error(Exception):
@@ -78,13 +112,18 @@ class Error(Exception):
 class HiraganaNotFoundError(Error):
 	pass #Exception when plate_hira is not in the list
 
+class LTOAbbreviationNotFoundError(Error):
+	pass #Exception when plate_hira is not in the list
+
 if __name__ == "__main__":
 	try:
-		p = Plate("横浜", "502", "の", "92-68", 1)
+		p = Plate("横浜", "330", "す", "10-74", 1)
 		print(p)
 		p.generatePlate()
 	except HiraganaNotFoundError:
 		print("Hiragana not supported")
+	except LTOAbbreviationNotFoundError:
+		print("LTO Abbreviation not supported")
 # plate_info = Plate("横浜", "200", "か", "11-74", 3)
 # plate_info.PLATE_TYPE
 # Plate.PLATE_TYPE
